4cf91eee16872342ff4ded35fea769dc7e2c5c1a
cwan <manwan_444@hotmail.com>
2017-09-08 00:53:06 +0700

    fix error

--- src/index.js
+++ src/index.js
@@ -27,12 +27,12 @@ bot.on('message', async message => {
     if (command.startsWith(prefix)) {
         let randomColor = '#000000'.replace(/0/g, function() { return (~~(Math.random() * 16)).toString(16); });
         //commands that start with !<command><arg>
-        if (command === `${prefix}ข้อมูลผู้ใช้`) {
+        if (command === `${prefix}ข้อมูลของฉัน`) {
             let embed = new Discord.RichEmbed()
-                .setTitle('ข้อมูลของผู้ใช้')
+                .setTitle(`ข้อมูลของ @${message.author.username}`)
                 .setImage(message.author.avatarURL)
                 .setColor(randomColor)
-                .addField('ชื่อผู้ใช้', `${message.author.username}#${message.author.discriminator}`)
+                .addField('Username', `${message.author.username}#${message.author.discriminator}`)
                 .addField('สร้างเมื่อ', message.author.createdAt);
             message.channel.sendEmbed(embed);
             message.client.sendEmbed(embed);
@@ -43,7 +43,7 @@ bot.on('message', async message => {
         //nomal commands <text>
         if (isNaN(message.content) ? !1 : (x = parseFloat(message.content), (0 | x) === x)) {
             let nextNum = parseInt(message.content) + 1;
-            message.reply(`และก็..${nextNum}...`);
+            message.reply(`..และก็..${nextNum}...`);
         }
 
         if (message.content === 'ping') {
