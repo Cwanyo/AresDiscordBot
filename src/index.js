abfdb667f944c0fa870d4e8bac0295181c0767e0
cwan <manwan_444@hotmail.com>
2017-09-08 00:36:13 +0700

    add userinfo func

--- src/index.js
+++ src/index.js
@@ -1,32 +1,64 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+const prefix = '!';
 
-client.on('ready', () => {
+bot.on('ready', async() => {
     console.log('Ares is Boot Up!');
+
+    try {
+        let link = await bot.generateInvite(['ADMINISTRATOR']);
+        console.log(link);
+    } catch (error) {
+        console.log(error.stack);
+    }
+
 });
 
-client.on('message', message => {
+bot.on('message', async message => {
     //ignore message from bot
     if (message.author.bot) {
         return;
     }
 
-    if (isNaN(message.content) ? !1 : (x = parseFloat(message.content), (0 | x) === x)) {
+    let messageArray = message.content.split(' ');
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
 
-        message.reply(parseInt(message.content) + 1);
+    if (command.startsWith(prefix)) {
+        let randomColor = '#000000'.replace(/0/g, function() { return (~~(Math.random() * 16)).toString(16); });
+        //commands that start with !<command><arg>
+        if (command === '${prefix}ข้อมูลผู้ใช้') {
+            let embed = new Discord.RichEmbed()
+                .setTitle('ข้อมูลของผู้ใช้')
+                .setImage(message.author.avatarURL)
+                .setColor(randomColor)
+                .addField('ชื่อผู้ใช้', '${message.author.username}#${message.author.discriminator}')
+                .addField('สร้างเมื่อ', message.author.createdAt);
+            message.channel.sendEmbed(embed);
+            message.client.sendEmbed(embed);
+            return;
+        }
+
+    } else {
+        //nomal commands <text>
+        if (isNaN(message.content) ? !1 : (x = parseFloat(message.content), (0 | x) === x)) {
+            let nextNum = parseInt(message.content) + 1;
+            message.reply('ตามด้วยย..${nextNum}...');
+        }
+
+        if (message.content === 'ping') {
+            message.reply('pong');
+        }
+
+        if (message.content === 'ไง') {
+            message.reply('โย้วววววว');
+        }
+
+        if (message.content === 'นอนๆ') {
+            message.reply('ฝันดี');
+        }
     }
 
-    if (message.content === 'ping') {
-        message.reply('pong');
-    }
-
-    if (message.content === 'ไง') {
-        message.reply('โย้วววววว');
-    }
-
-    if (message.content === 'นอนๆ') {
-        message.reply('ฝันดี');
-    }
 });
 
-client.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
